cb604f6309dc85ccaccf8e2c4853a397de02da09
Juan Manuel <jmcantone@gmail.com>
2021-01-08 18:04:55 +0100

    Se agrega guardado de imágenes
    Signed-off-by: Juan Manuel <jmcantone@gmail.com>

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -9,20 +9,37 @@ export function usePhotoGallery() {
 
     const [photos, setPhotos] = useState<Photo[]>([]);
     const { getPhoto } = useCamera();
+    const { deleteFile, getUri, readFile, writeFile } = useFilesystem();
+
   
     const takePhoto = async () => {
-      const cameraPhoto = await getPhoto({
-        resultType: CameraResultType.Uri,
-        source: CameraSource.Camera,
-        quality: 100
-      });
-      const fileName = new Date().getTime() + '.jpeg';
-        const newPhotos = [{
-        filepath: fileName,
-        webviewPath: cameraPhoto.webPath
-        }, ...photos];
-        setPhotos(newPhotos)
-    };
+        const cameraPhoto = await getPhoto({
+          resultType: CameraResultType.Uri,
+          source: CameraSource.Camera,
+          quality: 100
+        });
+      
+        const fileName = new Date().getTime() + '.jpeg';
+        const savedFileImage = await savePicture(cameraPhoto, fileName);
+        const newPhotos = [savedFileImage, ...photos];
+        setPhotos(newPhotos);
+      };
+
+    const savePicture = async (photo: CameraPhoto, fileName: string): Promise<Photo> => {
+        const base64Data = await base64FromPath(photo.webPath!);
+        const savedFile = await writeFile({
+          path: fileName,
+          data: base64Data,
+          directory: FilesystemDirectory.Data
+        });
+      
+        // Use webPath to display the new image instead of base64 since it's
+        // already loaded into memory
+        return {
+          filepath: fileName,
+          webviewPath: photo.webPath
+        };
+      };
   
     return {
       photos,
